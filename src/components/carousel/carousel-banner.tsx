b846b83ee286de8a14e92db4f19e8c9f0e1b1784
ABRAHAM LEANDRO VEGA <leandrovegaabraham@gmail.com>
2024-12-06 16:48:30 -0500

    first commit

---
+++ src/components/carousel/carousel-banner.tsx
@@ -0,0 +1,46 @@
+"use client";
+import * as React from "react";
+import Autoplay from "embla-carousel-autoplay";
+
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+
+export function CarouselBanner() {
+  const plugin = React.useRef(
+    Autoplay({ delay: 2000, stopOnInteraction: true })
+  );
+
+  return (
+    <Carousel
+      plugins={[plugin.current]}
+      className="w-full max-w-screen-2xl"
+      onMouseEnter={plugin.current.stop}
+      onMouseLeave={plugin.current.reset}
+    >
+      <CarouselContent>
+        {Array.from({ length: 5 }).map((_, index) => (
+          <CarouselItem key={index}>
+            <div className="p-2">
+              <Card>
+                <CardContent className="p-6">
+                  <img
+                    src="https://res.cloudinary.com/dmpmxzyrg/image/upload/v1732670625/Surprise_Dog_Trivia_2b06a5b28e.png"
+                    alt=""
+                  />
+                </CardContent>
+              </Card>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious className="-left-0" />
+      <CarouselNext className="-right-0" />
+    </Carousel>
+  );
+}
