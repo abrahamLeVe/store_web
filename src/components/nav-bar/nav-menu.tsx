5d6e2f20d804e225095328dd8ef21cdbd60fc424
ABRAHAM LEANDRO VEGA <leandrovegaabraham@gmail.com>
2024-12-10 21:57:03 -0500

    added eslint rules

--- src/components/nav-bar/nav-menu.tsx
+++ src/components/nav-bar/nav-menu.tsx
@@ -1,10 +1,8 @@
 "use client";
 
-import * as React from "react";
 import Link from "next/link";
+import * as React from "react";
 
-import { cn } from "@/lib/utils";
-// import { Icons } from "@/components/icons";
 import {
   NavigationMenu,
   NavigationMenuContent,
@@ -14,6 +12,7 @@ import {
   NavigationMenuTrigger,
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
+import { cn } from "@/lib/utils";
 import { LogIn } from "lucide-react";
 
 const components: { title: string; href: string; description: string }[] = [
@@ -64,7 +63,7 @@ export function NavMenu() {
             <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
               <li className="row-span-3">
                 <NavigationMenuLink asChild>
-                  <a
+                  <Link
                     className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
                     href="/"
                   >
@@ -77,7 +76,7 @@ export function NavMenu() {
                       paste into your apps. Accessible. Customizable. Open
                       Source.
                     </p>
-                  </a>
+                  </Link>
                 </NavigationMenuLink>
               </li>
               <ListItem href="/docs" title="Introduction">
